b7631bff4e672c3777787f8fd2813deee5267620
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-05 22:43:52 -0700

    Update differentiator.v

--- src/differentiator.v
+++ src/differentiator.v
@@ -1,19 +1,19 @@
-module differentiator(
+module differentiator (
     input wire clk,
     input wire rst,
     input wire [18:0] in,
     output reg [18:0] out
 );
 
-    reg [18:0] data;
+    reg [18:0] data = 0;
 
     always @(posedge clk or posedge rst) begin
-        if(rst) begin 
-	    out<=0;
-    	    data<=0;
-    	end else begin
-	    out<=in-data;
-	    data<=in;
+        if (rst) begin 
+            out <= 0;
+            data <= 0;
+        end else begin
+            out <= in - data;
+            data <= in;
         end
     end
 endmodule
