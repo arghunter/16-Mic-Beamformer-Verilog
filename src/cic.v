66760926a43ca1072a4f61edba3e5643c14c71b4
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-05 22:43:34 -0700

    Update cic.v

--- src/cic.v
+++ src/cic.v
@@ -1,50 +1,56 @@
-module cic(
+`include "incrementor.v"
+`include "integrator.v"
+`include "differentiator.v"
+module cic (
     input wire clk,
     input wire dec_clk,
     input wire rst,
     input wire in,
-    input reg [18:0] out 
+    output [18:0] out 
 );
 
     wire [18:0] inc_out;
     wire [18:0] int_1_out;
     wire [18:0] int_2_out;
+	wire [18:0] diff_1_out;
+	wire [18:0] diff_2_out;
     generate 
-        incrementor u_incrementor(
-    	    .clk(clk),
-	    .rst(rst),
-	    .in(in),
-	    .out(inc_out)
-        );
-	integrator u_integrator_1(
-	    .clk(clk),
-	    .rst(rst),
-	    .in(inc_out),
-	    .out(int_1_out)
-	);
-	integrator u_integrator_2(
-	    .clk(clk),
-	    .rst(rst),
-	    .in(int_1_out),
-	    .out(int_2_out)
-	);
-        differentiator u_differentiator_1(
-	    .clk(dec_clk),
-	    .rst(rst),
-	    .in(int_2_out),
-	    .out(diff_1_out)
-	);
-        differentiator u_differentiator_2(
-	    .clk(dec_clk),
-	    .rst(rst),
-	    .in(diff_1_out),
-	    .out(diff_2_out)
-	);
-        differentiator u_differentiator_1(
-	    .clk(dec_clk),
-	    .rst(rst),
-	    .in(diff_2_out),
-	    .out(out)
-	);
+		incrementor u_incrementor(
+			.clk(clk),
+			.rst(rst),
+			.in(in),
+			.out(inc_out)
+			);
+		integrator u_integrator_1(
+			.clk(clk),
+			.rst(rst),
+			.in(inc_out),
+			.out(int_1_out)
+		);
+		integrator u_integrator_2(
+			.clk(clk),
+			.rst(rst),
+			.in(int_1_out),
+			.out(int_2_out)
+		);
+		differentiator u_differentiator_1(
+			.clk(dec_clk),
+			.rst(rst),
+			.in(int_2_out),
+			.out(diff_1_out)
+		);
+		differentiator u_differentiator_2(
+			.clk(dec_clk),
+			.rst(rst),
+			.in(diff_1_out),
+			.out(diff_2_out)
+		);
+		differentiator u_differentiator_3(
+			.clk(dec_clk),
+			.rst(rst),
+			.in(diff_2_out),
+			.out(out)
+		);
     endgenerate
 
+endmodule
