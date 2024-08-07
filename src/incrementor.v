4af5f44da3267689e4d61d633a88b6527053aa2b
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-06 23:26:10 -0700

    Update incrementor.v

--- src/incrementor.v
+++ src/incrementor.v
@@ -4,20 +4,13 @@ module incrementor (
 	input wire rst,
 	output reg [18:0] out
 );
-
-	reg [18:0] data;
-	
-
 	always @(posedge clk or posedge rst) begin
 		if(rst) begin 
-			data<=0;
 			out<=0;	
 		end else if (in) begin
-			out<=data+1;
-			data<=data+1;
+			out<=out+1;
 		end else begin 
-			out<=data-1;
-			data<=data-1;
+			out<=out-1;
 		end
 	end
 endmodule
