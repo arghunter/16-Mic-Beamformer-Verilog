18af82a5f6c7e2e8c967a665475e96306851164b
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-10 16:18:03 -0700

    Update op_differentiator.v

--- src/op_differentiator.v
+++ src/op_differentiator.v
@@ -5,40 +5,48 @@ module op_differentiator (
     input wire [18:0] in,
     output reg [18:0] out
 );
-    reg [18:0] temp = 0;
-    reg [18:0] temp2 = 0;
-    reg [18:0] temp3 = 0;
-    reg [18:0] prev1 = 0;
-    reg [18:0] prev2 = 0;
-    reg [18:0] prev3 = 0;
-    reg [2:0] count=0;
-    always @(posedge rst) begin
-        if (rst) begin 
-            out <= 0;
+    reg [18:0] temp;
+    reg [18:0] prev1;
+    reg [18:0] prev2;
+    reg [18:0] prev3;
+    reg [2:0] count;
+    reg prev_lr_clk;
+
+    always @(posedge clk or posedge rst) begin
+
+        if (rst) begin
             prev1 <= 0;
             prev2 <= 0;
             prev3 <= 0;
-            temp<=0;
-    end
-    always @(posedge clk) begin
-            // USE A CASE STATEMENT
-        if(count == 0) begin
-            temp2<=in-prev1;
-            temp<=in;
-            count<=count+1;
-        end else if(count ==1) begin
-            prev1<=temp;
-            temp3<=temp2-prev2;
-            count<=count+1;
-        end else if(count==2) begin
-            out<=temp3-prev3;
-            prev2<=temp2;
-            count<=count+1;
-        end else begin  
-            prev3<=temp3;
+            temp<= 0; 
+            count<= 0; 
+            prev_lr_clk<= 0;     
+        end else begin
+            if(lr_clk && !prev_lr_clk ) begin
+                count<=0;
+            end else begin
+                case (count)
+                    0 : begin
+                        temp <= prev2;
+                        prev2<=in-prev1;
+                        prev1<=in;
+                        count<=count+1;
+                    end
+                    1 : begin
+                        
+                        prev3<=prev2-temp;
+                        temp<=prev3;
+                        count<=count+1;
+                    end
+                    2 : begin
+                        out<=prev3-temp;
+                        
+                        count<=count+1;
+                    end
+                endcase
+            end
+           prev_lr_clk <= lr_clk;
         end
     end
-    always @(posedge lr_clk) begin
-        count<=0;
-    end
+
 endmodule
