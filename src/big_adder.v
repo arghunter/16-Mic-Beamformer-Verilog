3a09d71e559b5c602b371d0c32b439d896b95eb5
Armaan Gomes <acgo8888@gmail.com>
2024-08-20 18:17:51 -0700

    stuff

---
+++ src/big_adder.v
@@ -0,0 +1,12 @@
+module big_adder(
+    input [23:0] in_0,
+    input [23:0] in_1,
+    input [23:0] in_2,
+    input [23:0] in_3,
+    input [23:0] in_4,
+    input [23:0] in_5,
+    input [23:0] in_6,
+    input [23:0] in_7,
+    input [23:0] in_8,
+    output [21:0] sum,
+);
