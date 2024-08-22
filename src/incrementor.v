571b123c874fd4d73f607e62fc96a44b83df3e13
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-21 22:22:54 -0700

    Update incrementor.v

--- src/incrementor.v
+++ src/incrementor.v
@@ -2,7 +2,7 @@ module incrementor (
 	input wire clk,
 	input wire in,
 	input wire rst,
-	output reg [18:0] out
+	output reg [23:0] out
 );
 	always @(posedge clk or posedge rst) begin
 		if(rst) begin 
