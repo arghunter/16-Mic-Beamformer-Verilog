04141f65084724aa96b36ae24fabee692af65d3d
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-08-21 22:22:36 -0700

    Update differentiator.v

--- src/differentiator.v
+++ src/differentiator.v
@@ -1,11 +1,11 @@
 module differentiator (
     input wire clk,
     input wire rst,
-    input wire [18:0] in,
-    output reg [18:0] out
+    input wire [23:0] in,
+    output reg [23:0] out
 );
 
-    reg [18:0] data = 0;
+    reg [23:0] data = 0;
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin 
